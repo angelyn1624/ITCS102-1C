a7bd0284b14f18dd9ac3b2045aab951ddcd7796e
angelyn1624 <angepimentel24@gmail.com>
2025-08-17 22:07:56 +0800

    Add files via upload

---
+++ code_challenge1.py
@@ -0,0 +1,4 @@
+name = input("type your name-->")
+
+print(" \t\t\t\t\t\t\t*  \n \t\t\t\t* \t\t\tHI! \t\t\t* \n \t\t* \t\t\t\t\t\t\t\t\t\t* \n \t\t\t* \t\t\t\t\t\t\t\t* \n \t\t\t\t*  \t\t\tangelyn \t\t* \n \t\t\t\t\t* \n \t\t\t\t\t\t*  \t\t\t* \n \t\t\t\t\t\t\t*   ", )
+  
