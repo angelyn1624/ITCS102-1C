9e75e5bda9509c229c693fd376cf517155c2c490
angelyn1624 <angepimentel24@gmail.com>
2025-08-19 21:16:12 +0800

    Add files via upload

---
+++ activity9.py
@@ -0,0 +1,15 @@
+a = 6
+b = 4
+
+c = 12
+d = 10
+
+print((a > b) and (c < d) )
+print((a < b) or ( c < d) )
+print( not(a < b) or (c < d))
+
+username = 'angelyn'
+password = 'cutie123'
+
+print("USER LOGIN ")
+print( (username == 'angelyn') and (password == 'cutie123'))
