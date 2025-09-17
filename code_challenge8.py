7d592ab01b1d8f0f77241633a5f6a50147b63f77
angelyn1624 <angepimentel24@gmail.com>
2025-09-17 17:46:59 +0800

    Add files via upload

---
+++ code_challenge8.py
@@ -0,0 +1,54 @@
+#multiplication table maker
+#ask the user to enter a number
+
+print("multiplication table maker")
+number = eval(input("enter a number:"))
+print("multiplication table for 6")
+
+for u in range (1,2,):
+	result = number * u
+
+print(f"{number} x {u} = {result}")
+for u in range (1,3,):
+	result = number * u
+
+print(f"{number} x {u} = {result}")
+for u in range (1,4,):
+	result = number * u
+
+print(f"{number} x {u} = {result}")
+for u in range (1,5,):
+	result = number * u
+
+print(f"{number} x {u} = {result}")
+for u in range (1,6,):
+	result = number * u
+
+print(f"{number} x {u} = {result}")
+for u in range (1,7,):
+	result = number * u
+
+print(f"{number} x {u} = {result}")
+for u in range (1,8,):
+	result = number * u
+
+print(f"{number} x {u} = {result}")
+for u in range (1,9,):
+	result = number * u
+
+print(f"{number} x {u} = {result}")
+for u in range (1,10,):
+	result = number * u
+
+print(f"{number} x {u} = {result}")
+for u in range (1,11,):
+	result = number * u
+
+print(f"{number} x {u} = {result}")
+
+
+
+
+	
+
+
