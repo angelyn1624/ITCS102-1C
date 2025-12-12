e898f03f41e17f1bd9e1dff40dfaa5467a7b54c2
angelyn1624 <angepimentel24@gmail.com>
2025-12-13 00:41:56 +0800

    Add files via upload

---
+++ activity25.py
@@ -0,0 +1,12 @@
+thai_series = ['glseries', 'romcom', 'runaway', 'gaptheseries', '23.5', 'reverse4you']
+
+print(thai_series[2])
+
+GLseries = {'lovesenior': 'nakakainlove', 'Queen': 'serious', 'safezone': 'romcom?'}
+
+print(GLseries['Queen'])
+
+
+
+
+
