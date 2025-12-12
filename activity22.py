b0de0ece79f499e2855fff1fdc63fbcd77b817ec
angelyn1624 <angepimentel24@gmail.com>
2025-12-13 00:41:28 +0800

    Add files via upload

---
+++ activity22.py
@@ -0,0 +1,8 @@
+
+month = ['jan', 'feb','march','april','may','june','july']
+
+print(month)
+
+month.reverse()
+print(month)
+
---
+++ activity23.py
@@ -0,0 +1,13 @@
+from activity23_1 import GreetName, GreetPerson, FunctionReturn, factorial
+
+def firstFunction():
+    print("how are you?")
+    print("gwenchana")
+    print("luh")
+
+firstFunction()
+GreetName("ange")
+GreetPerson("ange", "Silangan mayao", "23")
+
+print(f"sum of all  {FunctionReturn (7)}")
+print(f"sum of all {factorial(10) + 25}")
